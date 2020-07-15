cfbb998f49bf8f43c3a7d3c0c46dbddd5e4cc2bd
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-07-15 17:24:02 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,4 +26,10 @@ class SessionsController < ApplicationController
     # Ye olde redirect
     redirect_to root_path
   end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
 end
