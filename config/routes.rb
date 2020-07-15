5314fa18f9be3c96da46ff5c6a7b0962b37b17c7
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-07-15 17:22:37 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # Add your routes here
+
+  #this route allows us to redirect users to '/auth/:provider'
+  get '/auth/:provider/callback', to: 'sessions#create'
 end
