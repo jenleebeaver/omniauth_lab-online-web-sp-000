55f3efdb037b8e998bae39916732a62eca77a715
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-07-15 17:33:41 +0000

    Automatically backed up by Learn

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@
 #initializers hold configuration settings
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  #the providers are our third party authenticators
+  #the providers are our third party authenticators. :developer is our strategy.
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 
 end
