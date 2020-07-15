2b65e0fe710fa350f434caba0d147375a6d6800a
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-07-15 17:46:26 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,14 @@ class SessionsController < ApplicationController
   # strategy. We'll remove this when we move to a "real" provider.
   skip_before_action :verify_authenticity_token, only: :create
 
-  #below is code for create 
+  #below is code for create
   # def create
   #   @user = User.find_or_create_from_auth_hash(auth_hash)
   #   self.current_user = @user
   #   redirect_to '/'
   # end
 
-  def xcreate
+  def create
     # After entering a name and email value in the /auth/developer
     # path and submitting the form, you will see a pretty-print of
     # the authentication data object that comes from the "developer"
