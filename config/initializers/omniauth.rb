1579b8ae13c8cca02e5e2a9f62e67f8c9fae53ee
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-07-15 17:31:16 +0000

    Automatically backed up by Learn

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,4 +3,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+
+  .use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
 end
